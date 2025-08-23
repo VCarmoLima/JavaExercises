d268ef89eb7af52dd0f75e075299ac2f31486d64
VCarmoLima <viniciuscarmo.contato@gmail.com>
2025-08-23 10:00:10 -0300

    Exercício em Java que realiza a operação de média aritmética com três números. Utiliza a classe Scanner para obter a entrada do usuário e armazena os resultados das operações em variáveis.

---
+++ Exercises/src/C_ArithMean3Nums.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class C_ArithMean3Nums {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Qual o primeiro número?: ");
+        double numeroUm = scan.nextDouble();
+        System.out.print("Qual o segunda número?: ");
+        double numeroDois = scan.nextDouble();
+        System.out.print("Qual o terceiro número?: ");
+        double numeroTres = scan.nextDouble();
+
+        double soma = numeroUm + numeroDois + numeroTres;
+        double media = soma / 3;
+
+        System.out.printf("A média aritmética desses três números é de %s", media);
+
+        scan.close();
+    }
+}
