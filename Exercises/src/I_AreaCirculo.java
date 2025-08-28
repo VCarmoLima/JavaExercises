096191437a7468e05cd0949b217801f73c92f9d5
VCarmoLima <viniciuscarmo.contato@gmail.com>
2025-08-28 11:31:28 -0300

    feat: Adiciona funcionalidade para cálculo da área do círculo
    Implementa um script que calcula a área de um círculo a partir do raio fornecido pelo usuário.
    
    - Utiliza `Scanner` para a entrada de dados.
    - O cálculo da área é feito com `Math.PI` e `Math.pow` para maior precisão.
    - A saída é formatada com `printf` para exibir o resultado com duas casas decimais.

---
+++ Exercises/src/I_AreaCirculo.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class I_AreaCirculo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite o valor da raio: ");
+        double raio = sc.nextDouble();
+
+        double area = Math.PI * Math.pow(raio, 2);
+
+        System.out.printf("A área do círculo é:  %.2f", area);
+
+        sc.close();
+    }
+}
