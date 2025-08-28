dcd0e45eb31a2f93b868cb264c0ff8b1a710c71b
VCarmoLima <viniciuscarmo.contato@gmail.com>
2025-08-28 11:32:53 -0300

    feat: Implementa cálculo do perímetro do círculo
    Cria um programa que calcula o perímetro de um círculo com base no raio fornecido pelo usuário.
    
    - Utiliza a classe `Scanner` para a entrada de dados e `Math.PI` para o cálculo.
    - O resultado é exibido no console formatado com duas casas decimais.

---
+++ Exercises/src/H_PerimetroCirculo.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class H_PerimetroCirculo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite o raio do círculo: ");
+        double raio = sc.nextDouble();
+
+        double perimetro = (2 * Math.PI) * raio;
+
+        System.out.printf("O perímetro do círculo é de: %.2f%n", perimetro);
+
+        sc.close();
+    }
+}
