c39dd8b1406d07f45fbeda041fbd93178e595de2
VCarmoLima <viniciuscarmo.contato@gmail.com>
2025-08-28 11:30:21 -0300

    feat: Adiciona funcionalidade para cálculo da área do círculo
    Implementa um script que calcula a área de um círculo a partir do raio fornecido pelo usuário.
    
    - Utiliza `Scanner` para a entrada de dados.
    - O cálculo da área é feito com `Math.PI` e `Math.pow` para maior precisão.
    - A saída é formatada com `printf` para exibir o resultado com duas casas decimais.

---
+++ Exercises/src/L_CalculoTriangulo.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class L_CalculoTriangulo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite o lado A do triângulo: ");
+        double a = sc.nextDouble();
+        System.out.print("Digite o lado B do triângulo: ");
+        double b = sc.nextDouble();
+        System.out.print("Digite o lado C do triângulo: ");
+        double c = sc.nextDouble();
+        System.out.print("Digite a altura H relativa ao lado B do triângulo: ");
+        double h = sc.nextDouble();
+
+        double perimetro = (a + b + c);
+        double area = (b * h) / 2;
+
+        System.out.printf("O perímetro do triângulo é: %s%n", perimetro);
+        System.out.printf("A área do triângulo é: %s%n", area);
+
+        sc.close();
+    }
+}
